e7cfd509fae7fe44a9fb61e1e8284c6412118eaa
Dung Luong <dung291196@gmail.com>
2022-12-02 00:40:16 +0100

    Day 1

---
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using AdventOfCode2022;
+
+
+/*Day 1*/
+int value = Solutions.Day1(@"..\..\..\Inputs\Input_Day1.txt");
+Console.WriteLine(value);
+
+
+
+
---
+++ AdventOfCode2022/AdventOfCode2022/Solutions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AdventOfCode2022;
+
+public class Solutions
+{
+    /*=========================== Day 1 /*===========================*/
+    /*Solving this with simple brute force would be too easy.
+     Therefore, I tried to write a sollution in LINQ*/
+
+    public static int Day1(string filePath)
+    {
+        var elvesData = File.ReadAllText(filePath);
+
+        var result = elvesData
+            .Split("\n\n") /*Now we have a list of string, each string in the list contains the calories that an elf carries*/
+            .Select(stringWithNewLineChar =>
+                stringWithNewLineChar
+                    .Split("\n")) /*Turn the string represent the total calories of each elf into a list by spliting based on the new line character*/
+            .Select(arrayOfCalories => arrayOfCalories.Select(calory => Convert.ToInt32(calory)).Sum())
+            .OrderByDescending(values => values)
+            .First();
+
+        return result;
+    }
+
+    
+}
