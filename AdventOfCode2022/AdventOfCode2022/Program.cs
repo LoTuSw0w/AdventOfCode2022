40dc7bf0e881606db788f9374d1fc38e53c8b6e8
Dung Luong <dung291196@gmail.com>
2022-12-03 01:15:35 +0100

    Day 2 part 2

--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -6,12 +6,15 @@ using AdventOfCode2022;
 
 
 /*Day 1*/
-int day1 = Solutions.Day1(@"..\..\..\Inputs\Input_Day1.txt");
+int day1 = Solutions.Day1(@"..\..\..\Inputs\Input_Day1.txt", 3);
 Console.WriteLine($"Day 1: {day1}");
 
 /*Day 2*/
-int day2 = Solutions.Day2(@"..\..\..\Inputs\Input_Day2.txt");
-Console.WriteLine($"Day 1: {day2}");
+int day2part1 = Solutions.Day2_part1(@"..\..\..\Inputs\Input_Day2.txt");
+Console.WriteLine($"Day 2 part 1: {day2part1}");
+
+int day2part2 = Solutions.Day2_part2(@"..\..\..\Inputs\Input_Day2.txt");
+Console.WriteLine($"Day 2 part 2: {day2part2}");
 
 Console.ReadLine();
 
--- AdventOfCode2022/AdventOfCode2022/Solutions.cs
+++ AdventOfCode2022/AdventOfCode2022/Solutions.cs
@@ -5,11 +5,11 @@ namespace AdventOfCode2022;
 
 public class Solutions
 {
-    /*=========================== Day 1 /*===========================*/
+    /*=========================== Day 1 ===========================*/
     /*Solving this with simple brute force would be too easy.
-     Therefore, I tried to write a sollution in LINQ*/
+     Therefore, I tried to write a solution in LINQ*/
 
-    public static int Day1(string filePath)
+    public static int Day1(string filePath, int numberOfTopElves)
     {
         var elvesData = File.ReadAllText(filePath);
 
@@ -18,12 +18,14 @@ public class Solutions
             .Select(stringWithNewLineChar => stringWithNewLineChar .Split("\n")) /*Turn the string represent the total calories of each elf into a list by spliting based on the new line character*/
             .Select(arrayOfCalories => arrayOfCalories.Select(singleCalories => Convert.ToInt32(singleCalories)).Sum()) /*Convert each calories string into an integer, then get the sum of the list*/
             .OrderByDescending(values => values)
-            .First();
+            .Take(numberOfTopElves).Sum();
 
         return result;
     }
 
-    public static int Day2(string filePath)
+    /*=========================== Day 2 ===========================*/
+
+    public static int Day2_part1(string filePath)
     {
         Hashtable RPSMapping = new Hashtable();
         RPSMapping.Add("B X", 1);
@@ -37,9 +39,30 @@ public class Solutions
         RPSMapping.Add("B Z", 9);
 
         var elvesData = File.ReadAllText(filePath);
-        var result = elvesData.Split("\n")
+        var result = elvesData.Split("\r\n")
         .Select(data => RPSMapping[data])
         .Sum(x => Convert.ToInt32(x));
+        return result;
+    }
+    /*(1 for Rock A, 2 for Paper B , and 3 for Scissors C)*/
+    /*(0 if you lost, 3 if the round was a draw, and 6 if you won)*/
+    public static int Day2_part2(string filePath)
+    {
+        Hashtable RPSMapping = new Hashtable();
+        RPSMapping.Add("A X", 3);
+        RPSMapping.Add("A Y", 4);
+        RPSMapping.Add("A Z", 8);
+        RPSMapping.Add("B X", 1);
+        RPSMapping.Add("B Y", 5);
+        RPSMapping.Add("B Z", 9);
+        RPSMapping.Add("C X", 2);
+        RPSMapping.Add("C Y", 6);
+        RPSMapping.Add("C Z", 7);
+
+        var elvesData = File.ReadAllText(filePath);
+        var result = elvesData.Split("\r\n")
+            .Select(data => RPSMapping[data])
+            .Sum(x => Convert.ToInt32(x));
         return result;
     }
 
